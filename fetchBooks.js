a10ff2ff21afcb5226e162010e5099c525d8665c
Simran <simrantamang6008@gmail.com>
2024-09-03 09:15:58 +0530

    complete phase 2

--- fetchBooks.js
+++ fetchBooks.js
@@ -1,46 +1,35 @@
-const axios = require('axios');
+const readline = require('node:readline');
 const fs = require('fs');
-const isbnList = [ // Example ISBNs
-    '9780679783268',
-    '9780307476463',
-    '9781581212105',
-    '9780321625922',
-    '5367255272676',
-    '9781133112280',
-    '9780205957606'
-];
 
-const fetchBookInfo = async (isbn) => {
-    try {
-        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
-        //console.log(response)
-        if (response.data.totalItems > 0) {
-            const book = response.data.items[0].volumeInfo;
-            //console.log(book)
-            return {
-                title: book.title,
-                authors: book.authors,
-                publisher: book.publisher,
-                language: book.language,
-            };
-        } else {
-            //console.log("No information found for ISBN");
-        }
-    } catch (error) {
-        console.error(error);
-    }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const booksData = [];
+
+// Function to ask for book details from the user
+const askBookDetails = () => {
+  rl.question('Enter book name: ', (title) => {
+    rl.question('Enter author name: ', (author) => {
+      const bookInfo = {
+        title: title,
+        authors: [author]
+      };
+      booksData.push(bookInfo);
+
+      saveBooksData();
+      rl.close();
+    });
+  });
 };
 
-const fetchBooksData = async () => {
-    const booksData = [];
-    for (let isbn of isbnList) {
-        const bookInfo = await fetchBookInfo(isbn);
-        if (bookInfo) {
-            booksData.push(bookInfo);
-        }
-    }
-    fs.writeFileSync('booksData.json', JSON.stringify(booksData,null, 2));
-    console.log('Books details saved to json file');
+// Function to save book data to JSON file
+const saveBooksData = () => {
+  fs.writeFileSync('booksData.json', JSON.stringify(booksData, null, 2));
+  console.log('Books details saved to booksData.json');
+  console.log('Here are the details:');
+  console.log(JSON.stringify(booksData, null, 2));
 };
 
-fetchBooksData();
+askBookDetails();
